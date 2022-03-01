72a614c5be1becb034f95598c32071a8ce83c92d
k-e-r <damajonboua@gmail.com>
2022-02-28 16:52:41 -0800

    for publish

---
+++ build/precache-manifest.7704ddf94d56a7cbe27954566e0bbef4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15175bfa36ca867de864904861693fbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cddbe60c32f2de41b83d",
+    "url": "/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "1c608271932903ae005b",
+    "url": "/static/js/2.7dfd526b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7dfd526b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cddbe60c32f2de41b83d",
+    "url": "/static/js/main.c64eb336.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
